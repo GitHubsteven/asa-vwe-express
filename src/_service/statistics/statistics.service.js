a7f9824f8374f3313ff8d4f82286471f484615a2
rongbin.xie <rongbin.xie@voyageone.cn>
2020-10-11 14:32:46 +0800

    增加按时间统计接口

--- src/_service/statistics/statistics.service.js
+++ src/_service/statistics/statistics.service.js
@@ -2,10 +2,22 @@ let BlogModel = require('../../models/blog.model');
 // let CommentModel = require('../../models/comment.model');
 
 module.exports = {
-    blogTypeStatistics
+    blogTypeStatistics,
+    blogCreateTimeStatistics
 };
 
 async function blogTypeStatistics() {
     return await BlogModel.aggregate([])
-      .group({"_id": "$categories", count: {$sum: 1}});
+      .group({
+          "_id": "$categories",
+          count: {$sum: 1}
+      });
+}
+
+async function blogCreateTimeStatistics() {
+    return await BlogModel.aggregate([])
+      .group({
+          "_id": {$dateToString: {format: "%Y-%m", date: "$createTime"}},
+          count: {$sum: 1}
+      });
 }
